11e67993a1e9022e36a2fd777355ef4fdb1703c0
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-12 10:05:44 +0900

    Create opEx4_04.c

---
+++ step2/opEx4_04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//// 관계 연산자
+
+void main()
+{
+	int a, b;
+
+	a = 10;
+	b = 11;
+
+	printf("\n a>b  = %d", a > b);
+	printf("\n a<b  = %d", a < b);
+	printf("\n a>=b = %d", a >= b);
+	printf("\n a<=b = %d", a <= b);
+	printf("\n a==b = %d", a == b);
+	printf("\n a!=b = %d", a != b);
+}
